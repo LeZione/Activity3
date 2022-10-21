5c3ac727f74f18de803ea21bc412a380055f0b87
LeZione <eprado706@gmail.com>
2022-10-21 12:36:51 +0800

    initial commit

---
+++ name.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="design.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Gotu&family=Licorice&family=Livvic:wght@100&family=Lobster&family=M+PLUS+Code+Latin&family=Source+Code+Pro&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class = "content">
+            <div class ="greetings">
+                <?php
+                echo "Hello:";
+                ?>
+            </div>
+            <div class = "name">
+                <?php
+                echo "<span class ='uno'>Keith Emmanuel M. Prado.</span>";
+                echo "<span class ='dos'> L E Z I O N E.</span> ";
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
